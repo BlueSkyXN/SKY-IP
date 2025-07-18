89cb4e579eadf8bf98e6617f131714439a1a5614
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2025-07-18 17:16:23 +0800

    0.2.1

---
+++ functions/api/ip/[ip].js
@@ -0,0 +1,13 @@
+export async function onRequest(context) {
+  const ip = context.params.ip;
+  const url = `https://apimobile.meituan.com/locate/v2/ip/loc?rgeo=true&ip=${ip}`;
+  const response = await fetch(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+  });
+  const data = await response.json();
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
